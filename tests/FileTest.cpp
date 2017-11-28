7f79a58c53a138ca30d6fd4967d5ab563716261c
Bartłomiej Kordek <bartekordek@gmail.com>
2017-11-28 16:07:41 +0100

    Added new file test, renamed old, updated binaries.

--- tests/FileTest.cpp
+++ tests/FileTest.cpp
@@ -2,6 +2,18 @@
 
 #include "CUL/IFile.hpp"
 
+TEST_F(FileTest, fileFileExtistFileReturnsTrue)
+{
+    auto f = CUL::FS::IFile::createFile(dummyFilePath);
+    GTEST_ASSERT_EQ(true, f->exists());
+}
+
+TEST_F(FileTest, fileFileDontExtistFileReturnsFalse)
+{
+    auto f = CUL::FS::IFile::createFile(std::string("someNotExistingFile.exe"));
+    GTEST_ASSERT_EQ(false, f->exists());
+}
+
 TEST_F( FileTest, basicFileLoadFirstLine )
 {
 	auto f = CUL::FS::IFile::createFile( dummyFilePath );
@@ -14,10 +26,4 @@ TEST_F( FileTest, basicFileLoadlastLine )
 	auto f = CUL::FS::IFile::createFile( dummyFilePath );
 	f->load();
 	GTEST_ASSERT_EQ( "Line3", f->lastLine()->getVal() );
-}
-
-TEST_F( FileTest, existence )
-{
-	auto f = CUL::FS::IFile::createFile( dummyFilePath );
-	GTEST_ASSERT_EQ( true, f->exists() );
 }
