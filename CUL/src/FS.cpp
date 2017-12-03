e66847e3d1f180b1d8759c4cdcef2bf8ea443576
Bartłomiej Kordek <bartekordek@gmail.com>
2017-12-03 18:47:04 +0100

    Build fix.

--- CUL/src/FS.cpp
+++ CUL/src/FS.cpp
@@ -1,4 +1,5 @@
 #include "CUL/FS.hpp"
+#include <boost/filesystem.hpp>
 using namespace CUL;
 using namespace FS;
 
@@ -9,4 +10,10 @@ FSApi::FSApi()
 
 FSApi::~FSApi()
 {
+}
+
+std::string FSApi::getCurrentDir()
+{
+    boost::filesystem::path full_path(boost::filesystem::current_path());
+    return full_path.string();
 }
