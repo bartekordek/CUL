b95b7efd1a81e6408fff5b085c4ec9bb80f2ef4a
Bartłomiej Kordek <bartekordek@gmail.com>
2017-12-12 13:12:51 +0100

    Added tests.

---
+++ tests/FilesystemTests.cpp
@@ -0,0 +1,50 @@
+#include "FilesystemTests.hpp"
+#include "CUL/Path.hpp"
+#include "CUL/FS.hpp"
+#include <iostream>
+
+using Path = CUL::FS::Path;
+
+FilesystemTests::FilesystemTests()
+{
+    auto cwd = CUL::FS::FSApi::getCurrentDir();
+    std::cout << "Current dir: " << cwd << "\n";
+}
+
+TEST_F( FilesystemTests, GetExtension )
+{
+    Path file( "SomeDirectory/SomeFile.extension" );
+    ASSERT_EQ( file.getExtension(), ".extension" );
+}
+
+TEST_F( FilesystemTests, GetDirectory )
+{
+    Path file( "SomeDirectory/SomeFile.extension" );
+    ASSERT_EQ( file.getDir(), "SomeDirectory" );
+}
+
+TEST_F( FilesystemTests, GetBaseName )
+{
+    Path file( "SomeDirectory/SomeFile.extension" );
+    ASSERT_EQ( file.getBaseName(), "SomeFile" );
+}
+
+TEST_F( FilesystemTests, GetFullPath )
+{
+    Path file( "C:\\SomeDirectory\\SomeFile.extension" );
+    ASSERT_EQ( file.getPath(), "C:\\SomeDirectory\\SomeFile.extension" );
+}
+
+TEST_F( FilesystemTests, FileExistence )
+{
+    std::string filePath = "../media/Dummy.txt";
+    std::cout << "Trying to find: " << filePath << std::endl;
+    Path file( filePath.c_str() );
+    ASSERT_EQ( file.exists(), true );
+}
+
+TEST_F( FilesystemTests, FileNotExist )
+{
+    Path file( "C:\\Windows\\System32\\xcopy312312.exe" );
+    ASSERT_EQ( file.exists(), false );
+}
