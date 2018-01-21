be0fae02d427bf5290bdb413706855e2976f128e
Bart³omiej Kordek <bartekordek@gmail.com>
2018-01-21 17:39:42 +0100

    Added typedefs.

--- CUL/inc/CUL/Math/XYPosition.hpp
+++ CUL/inc/CUL/Math/XYPosition.hpp
@@ -1,17 +1,23 @@
 #pragma once
 namespace CUL
 {
-	template <typename Type>
-	struct XYPosition
-	{
-		XYPosition( const Type x = static_cast<Type>( 0 ), const Type y = static_cast<Type>( 0 ) ):
-			x( x ),
-			y( y )
-		{
+    template <typename Type>
+    struct Position2D
+    {
+        Position2D( 
+            const Type x = static_cast<Type>( 0 ), 
+            const Type y = static_cast<Type>( 0 ) ):
+            x( x ),
+            y( y )
+        {
 
-		}
+        }
 
-		Type x = static_cast<Type>( 0 );
-		Type y = static_cast<Type>( 0 );
-	};
+        Type x = static_cast<Type>( 0 );
+        Type y = static_cast<Type>( 0 );
+    };
+
+    using Position2DI = Position2D<int>;
+    using Position2DU = Position2D<unsigned>;
+    using Position2DD = Position2D<double>;
 }
