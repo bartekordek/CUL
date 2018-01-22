e54ad0ee73b1595b34267d7af7a1dc805d012292
Bartłomiej Kordek <bartekordek@gmail.com>
2018-01-22 15:09:56 +0100

    Updated Pivot class.

--- CUL/inc/CUL/IPivot.hpp
+++ CUL/inc/CUL/IPivot.hpp
@@ -1,9 +1,5 @@
 #pragma once
-
-/*
-
-*/
-
+#include "CUL/Math/Vector3D.hpp"
 namespace CUL
 {
     class IPivot
@@ -12,14 +8,12 @@ namespace CUL
         IPivot();
         virtual ~IPivot();
 
-        virtual void setPivotNorm( const double px, const double py, const double pz ) = 0;
-        virtual void setPivotNormX( const double val ) = 0;
-        virtual void setPivotNormY( const double val ) = 0;
-        virtual void setPivotNormZ( const double val ) = 0;
+        void setPivotNorm( const double px, const double py, const double pz );
+        void setPivotNormX( const double val );
+        void setPivotNormY( const double val );
+        void setPivotNormZ( const double val );
 
-        virtual const double getNormPivotX()const = 0;
-        virtual const double getNormPivotY()const = 0;
-        virtual const double getNormPivotZ()const = 0;
+        const CUL::Math::Vector3Du& getPivotNormalized()const;
 
         virtual void setPivotAbs( const double px, const double py, const double pz ) = 0;
         virtual void setPivotAbsX( const double val ) = 0;
@@ -31,8 +25,10 @@ namespace CUL
         virtual const double getAbsPivotZ()const = 0;
 
     protected:
+        virtual void calculateAbsPivot() = 0;
 
     private:
+        CUL::Math::Vector3Du m_pNorm = CUL::Math::Vector3Dd( 0.5, 0.5, 0.5 );
 
     };
 }
--- CUL/src/IPivot.cpp
+++ CUL/src/IPivot.cpp
@@ -6,7 +6,31 @@ IPivot::IPivot()
 {
 }
 
-
 IPivot::~IPivot()
 {
+}
+
+void IPivot::setPivotNorm( const double px, const double py, const double pz )
+{
+    calculateAbsPivot();
+}
+
+void IPivot::setPivotNormX( const double val )
+{
+    calculateAbsPivot();
+}
+
+void IPivot::setPivotNormY( const double val )
+{
+    calculateAbsPivot();
+}
+
+void IPivot::setPivotNormZ( const double val )
+{
+    calculateAbsPivot();
+}
+
+const CUL::Math::Vector3Du& IPivot::getPivotNormalized()const
+{
+    return this->m_pNorm;
 }
