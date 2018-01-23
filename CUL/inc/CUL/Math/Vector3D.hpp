714468d5c7bc0f29829eb9b06207370ea89d7689
Bartłomiej Kordek <bartekordek@gmail.com>
2018-01-23 16:56:55 +0100

    Updated Vector3D class.

--- CUL/inc/CUL/Math/Vector3D.hpp
+++ CUL/inc/CUL/Math/Vector3D.hpp
@@ -3,12 +3,12 @@
 #include "CUL/CUL.hpp"
 namespace CUL
 {
-	namespace Math
-	{
-		template <typename Type>
-		class Vector3D: public Epsilon<Type>
-		{
-		public:
+    namespace Math
+    {
+        template <typename Type>
+        class Vector3D: public Epsilon<Type>
+        {
+        public:
             enum class Axis : short
             {
                 X,
@@ -16,126 +16,134 @@ namespace CUL
                 Z
             };
 
-			Vector3D() = default;
-			Vector3D( const Type xVal, const Type yVal, const Type zVal ):
-				x(xVal), 
-				y(yVal),
-				z(zVal)
-			{
-			}
+            Vector3D() = default;
+            Vector3D( const Type xVal, const Type yVal, const Type zVal ):
+                x(xVal), 
+                y(yVal),
+                z(zVal)
+            {
+            }
 
-			Vector3D( const Vector3D<Type>& vector ) = default;
+            Vector3D( const Vector3D<Type>& vector ) = default;
 
-			virtual ~Vector3D() = default;
+            template <class someOtherClass>
+            Vector3D( const Vector3D<someOtherClass>& right ):
+                x( static_cast<Type>( right.getX() ) ),
+                y( static_cast<Type>( right.getY() ) ),
+                z( static_cast<Type>( right.getZ() ) )
+            {
+            }
 
-			const Type getX()const
-			{
-				return this->x;
-			}
+            virtual ~Vector3D() = default;
 
-			const Type getY()const
-			{
-				return this->y;
-			}
+            const Type getX()const
+            {
+                return this->x;
+            }
 
-			const Type getZ()const
-			{
-				return this->z;
-			}
+            const Type getY()const
+            {
+                return this->y;
+            }
 
-			void setX( const Type xValue )
-			{
-				this->x = xValue;
-			}
+            const Type getZ()const
+            {
+                return this->z;
+            }
 
-			void setY( const Type yValue )
-			{
-				this->y = yValue;
-			}
+            void setX( const Type xValue )
+            {
+                this->x = xValue;
+            }
 
-			void setZ( const Type zValue )
-			{
-				this->z = zValue;
-			}
+            void setY( const Type yValue )
+            {
+                this->y = yValue;
+            }
 
-			void setXYZ( const Type xVal, const Type yVal, const Type zVal )
-			{
-				this->x = xVal;
-				this->y = yVal;
-				this->z = zVal;
-			}
+            void setZ( const Type zValue )
+            {
+                this->z = zValue;
+            }
 
-			void setXYZ( const Vector3D& object )
-			{
-				this->x = object.x;
-				this->y = object.y;
-				this->z = object.z;
-			}
+            void setXYZ( const Type xVal, const Type yVal, const Type zVal )
+            {
+                this->x = xVal;
+                this->y = yVal;
+                this->z = zVal;
+            }
 
-			const bool operator==( const Vector3D& ivector )const
-			{
-				if( 
-					Epsilon<Type>::equals( this->x, ivector.x ) &&
-					Epsilon<Type>::equals( this->y, ivector.y ) &&
-					Epsilon<Type>::equals( this->z, ivector.z ) )
-				{
-					return true;
-				}
-				return false;
-			}
+            void setXYZ( const Vector3D& object )
+            {
+                this->x = object.x;
+                this->y = object.y;
+                this->z = object.z;
+            }
 
-			Vector3D<Type> operator*( const Vector3D<Type>& t )const
-			{
-				Vector3D<Type> result;
-				result.x = this->x * t.x;
-				result.y = this->y * t.y;
-				result.z = this->z * t.z;
-				return result;
-			}
+            const bool operator==( const Vector3D& ivector )const
+            {
+                if( 
+                    Epsilon<Type>::equals( this->x, ivector.x ) &&
+                    Epsilon<Type>::equals( this->y, ivector.y ) &&
+                    Epsilon<Type>::equals( this->z, ivector.z ) )
+                {
+                    return true;
+                }
+                return false;
+            }
 
-			Vector3D<Type>& operator*=( const Vector3D<Type>& t )
-			{
-				this->x = this->x * t.x;
-				this->y = this->y * t.y;
-				this->z = this->z * t.z;
-				return *this;
-			}
+            Vector3D<Type> operator*( const Vector3D<Type>& t )const
+            {
+                Vector3D<Type> result;
+                result.x = this->x * t.x;
+                result.y = this->y * t.y;
+                result.z = this->z * t.z;
+                return result;
+            }
 
-			Vector3D<Type> operator*( const Type& t )const
-			{
-				Vector3D<Type> result;
-				result.x = this->x * t;
-				result.y = this->y * t;
-				result.z = this->z * t;
-				return result;
-			}
+            Vector3D<Type>& operator*=( const Vector3D<Type>& t )
+            {
+                this->x = this->x * t.x;
+                this->y = this->y * t.y;
+                this->z = this->z * t.z;
+                return *this;
+            }
 
-			Vector3D<Type>& operator*=( const Type& t )
-			{
-				this->x = this->x * t;
-				this->y = this->y * t;
-				this->z = this->z * t;
-				return *this;
-			}
+            Vector3D<Type> operator*( const Type& t )const
+            {
+                Vector3D<Type> result;
+                result.x = this->x * t;
+                result.y = this->y * t;
+                result.z = this->z * t;
+                return result;
+            }
 
-			Vector3D<Type>& operator+=( const Vector3D<Type>& right )
-			{
-				this->x += right.x;
-				this->y += right.y;
-				this->z += right.z;
-				return *this;
-			}
+            Vector3D<Type>& operator*=( const Type& t )
+            {
+                this->x = this->x * t;
+                this->y = this->y * t;
+                this->z = this->z * t;
+                return *this;
+            }
 
-			Vector3D<Type>& operator=( const Vector3D<Type>& right )
-			{
-				if( this != &right )
-				{
-					this->x = right.x;
-					this->y = right.y;
-					this->z = right.z;
-				}
-				return *this;
-			}
+            Vector3D<Type>& operator+=( const Vector3D<Type>& right )
+            {
+                this->x += right.x;
+                this->y += right.y;
+                this->z += right.z;
+                return *this;
+            }
+
+            Vector3D<Type>& operator=( const Vector3D<Type>& right )
+            {
+                if( this != &right )
+                {
+                    this->x = right.x;
+                    this->y = right.y;
+                    this->z = right.z;
+                }
+                return *this;
+            }
 
             Vector3D<Type> operator/( const Vector3D<Type>& t )const
             {
@@ -179,16 +187,16 @@ namespace CUL
                 return xx * yy * zz;
             }
 
-			const Vector3D<Type> crossProducti( const Vector3D<Type>& right )const
-			{
-				const auto& v1 = *this;
-				const auto& v2 = right;
+            const Vector3D<Type> crossProducti( const Vector3D<Type>& right )const
+            {
+                const auto& v1 = *this;
+                const auto& v2 = right;
 
-				const Type cx = v1.y * v2.z - v1.z * v2.y;
-				const Type cy = v1.z * v2.x - v1.x * v2.z;
-				const Type cz = v1.x * v2.y - v1.y * v2.x;
-				return Vector3D<Type>( cx, cy, cz );
-			}
+                const Type cx = v1.y * v2.z - v1.z * v2.y;
+                const Type cy = v1.z * v2.x - v1.x * v2.z;
+                const Type cz = v1.x * v2.y - v1.y * v2.x;
+                return Vector3D<Type>( cx, cy, cz );
+            }
 
             const Type getAxis( const Axis axis )const
             {
@@ -207,14 +215,24 @@ namespace CUL
                 return static_cast<Type>(0);
             }
 
-		protected:
-			Type x = static_cast<Type>( 0 );
-			Type y = static_cast<Type>( 0 );
-			Type z = static_cast<Type>( 0 );
-		};
+            template <class someOtherClass>
+            Vector3D<Type>& operator=( const Vector3D<someOtherClass>& right )
+            {
+                this->x = static_cast<Type>( right.x );
+                this->y = static_cast<Type>( right.y );
+                this->z = static_cast<Type>( right.z );
+                return *this;
+            }
 
-		using Vector3Dd = Vector3D<double> ;
-		using Vector3Di = Vector3D<int>;
-		using Vector3Du = Vector3D<unsigned>;
-	}
+        protected:
+        private:
+            Type x = static_cast<Type>( 0 );
+            Type y = static_cast<Type>( 0 );
+            Type z = static_cast<Type>( 0 );
+        };
+
+        using Vector3Dd = Vector3D<double> ;
+        using Vector3Di = Vector3D<int>;
+        using Vector3Du = Vector3D<unsigned>;
+    }
 }
