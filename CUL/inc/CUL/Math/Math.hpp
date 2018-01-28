76c58f43eed354f1442f7aba9a6294e94c73ca97
Bart³omiej Kordek <bartekordek@gmail.com>
2018-01-28 15:30:42 +0100

    Replaced tabs with spaces, added PI const.

--- CUL/inc/CUL/Math/Math.hpp
+++ CUL/inc/CUL/Math/Math.hpp
@@ -1,19 +1,24 @@
 #pragma once
 namespace CUL
 {
-	namespace Math
-	{
-		namespace Util
-		{
-			template<typename Type>
-			Type abs( const Type arg )
-			{
-				if( arg < 0 )
-				{
-					return 0 - arg;
-				}
-				return arg;
-			}
-		}
-	}
+    namespace Math
+    {
+        namespace Util
+        {
+            template<typename Type>
+            Type abs( const Type arg )
+            {
+                if( arg < 0 )
+                {
+                    return 0 - arg;
+                }
+                return arg;
+            }
+        }
+
+        namespace Constants
+        {
+            const double PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481;
+        }
+    }
 }
