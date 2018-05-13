f16c2cac610f4844db6e38cc699f60a2fc102bdb
Bart³omiej Kordek <bartekordek@gmail.com>
2018-05-13 17:49:20 +0200

    Extracted arguments as object.

--- libcul/inc/CUL/ConsoleUtilities.hpp
+++ libcul/inc/CUL/ConsoleUtilities.hpp
@@ -1,10 +1,24 @@
 #pragma once
+
 #include "CUL.hpp"
+
+#include <utility>
+
 namespace CUL
 {
+    struct CULLib_API ArgumentsPair
+    {
+        int argc = 0;
+        char** argv = nullptr;
+    };
+
     class CULLib_API ConsoleUtilities
     {
     public:
-        static void PrintInputParameters( int argc, char *argv[] );
+        static void printInputParameters( const ArgumentsPair& arguments );
+        static ArgumentsPair& getDefaultArgs();
+
+    private:
+        static ArgumentsPair s_args;
     };
 }
--- libcul/src/ConsoleUtilities.cpp
+++ libcul/src/ConsoleUtilities.cpp
@@ -2,13 +2,30 @@
 
 #include <iostream>
 
-namespace CUL
+using namespace CUL;
+
+    ArgumentsPair ConsoleUtilities::s_args = { 0, nullptr };
+
+void ConsoleUtilities::printInputParameters( const ArgumentsPair& args )
 {
-    void ConsoleUtilities::PrintInputParameters( int argc, char *argv[] )
+    for( int i = 0; i < args.argc; ++i )
     {
-        for( int i = 0; i < argc; ++i )
-        {
-            std::cout << "ARG[" << i << "] = " << argv[i] << std::endl;
-        }
+        std::cout << "ARG[" << i << "] = " << args.argv[i] << "\n";
     }
+}
+
+ArgumentsPair& ConsoleUtilities::getDefaultArgs()
+{
+    if( nullptr == s_args.argv )
+    {
+        s_args.argc = 2;
+        s_args.argv = new char*[ 2 ];
+
+        s_args.argv[ 0 ] = new char[ 6 ];
+        s_args.argv[ 0 ] = "12345\0";
+
+        s_args.argv[ 1 ] = new char[ 5 ];
+        s_args.argv[ 1 ] = "Test\0";
+    }
+    return s_args;
 }
