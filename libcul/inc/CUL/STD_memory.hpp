078c50e03fe41473d16b4987522819dad9900089
Bart³omiej Kordek <bartekordek@gmail.com>
2018-06-24 20:00:34 +0200

    Added msvc guard.

--- libcul/inc/CUL/STD_memory.hpp
+++ libcul/inc/CUL/STD_memory.hpp
@@ -3,6 +3,7 @@
 #pragma warning( push, 0 )
 #pragma warning( disable: 4365 )
 #pragma warning( disable: 4514 )
+#pragma warning( disable: 4571 )
 #pragma warning( disable: 4820 )
 #pragma warning( disable: 5045 )
 #endif
