c39033541a898415382eb0b45bcf2314266f999d
Bartłomiej Kordek <bartekordek@gmail.com>
2018-06-28 15:49:01 +0200

    Turned out 3 more msvc errors, changed file permissions.

--- libcul/inc/CUL/STD_set.hpp
+++ libcul/inc/CUL/STD_set.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #if _MSC_VER
 #pragma warning( push, 0 )
+#pragma warning( disable: 4571 )
+#pragma warning( disable: 4625 )
+#pragma warning( disable: 4820 )
 #endif
 #include <set>
 #ifdef _MSC_VER
