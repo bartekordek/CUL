6db670400951073bbbf4c8d27e7ddcffc48ffca4
Bartłomiej Kordek <bartekordek@gmail.com>
2018-07-08 19:44:45 +0200

    Added cerr call.

--- libcul/src/SimpleAssert.cpp
+++ libcul/src/SimpleAssert.cpp
@@ -1,9 +1,15 @@
 #include "CUL/SimpleAssert.hpp"
+#include "CUL/STD_iostream.hpp"
+
 #include "boost/assert.hpp"
 
 using namespace CUL;
 
 void Assert::simple( const bool val, const std::string& msg )
 {
-    BOOST_ASSERT_MSG( val, msg.c_str() );
+    if( 0 == val )
+    {
+        std::cerr << msg << "\n";
+        BOOST_ASSERT_MSG( val, msg.c_str() );
+    }
 }
