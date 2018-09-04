1452f1b3e581e0636e7a81fc34d3aca0836e49c2
Bartłomiej Kordek <bartekordek@gmail.com>
2018-09-04 12:51:51 +0200

    Added more pragmas to mutex [msvc compilation].

--- libcul/inc/CUL/STD_mutex.hpp
+++ libcul/inc/CUL/STD_mutex.hpp
@@ -3,9 +3,11 @@
 #pragma warning( push, 0 )
 #pragma warning( disable: 4365 )
 #pragma warning( disable: 4514 )
+#pragma warning( disable: 4623 )
 #pragma warning( disable: 4625 )
 #pragma warning( disable: 4626 )
 #pragma warning( disable: 4820 )
+#pragma warning( disable: 5027 )
 #pragma warning( disable: 5039 )
 #endif
 #include <mutex>
