568525372de647811e520b36c4a88f11664d1e58
Bartłomiej Kordek <bartekordek@gmail.com>
2018-10-01 18:49:29 +0200

    Added Frame counter class.

---
+++ libcul/inc/CUL/Video/IFPSCounter.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "CUL/UselessMacros.hpp"
+#include "CUL/CUL.hpp"
+
+NAMESPACE_BEGIN( CUL )
+NAMESPACE_BEGIN( Video )
+
+class CULLib_API IFPSCounter
+{
+public:
+    IFPSCounter();
+    virtual ~IFPSCounter();
+
+    virtual void start() = 0;
+    virtual void stop() = 0;
+    virtual void increase() = 0;
+    virtual const double getCurrentFps()const = 0;
+    virtual const double getAverageFps()const = 0;
+};
+
+class CULLib_API FPSCounterFactory
+{
+public:
+    static IFPSCounter* getConcreteFPSCounter();
+protected:
+private:
+};
+
+NAMESPACE_END( Video )
+NAMESPACE_END( CUL )
