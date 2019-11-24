0760d66e19cc7e824292e0af13c89f84df0467ee
Bartłomiej Kordek <bartekordek@gmail.com>
2019-11-24 18:42:18 +0100

    Replace namespace block with macro.

--- libcul/inc/CUL/ITimer.hpp
+++ libcul/inc/CUL/ITimer.hpp
@@ -1,28 +1,31 @@
 #pragma once
+
 #include "CUL/CUL.hpp"
 #include "CUL/ITime.hpp"
-namespace CUL 
+
+NAMESPACE_BEGIN( CUL )
+
+class CULLib_API ITimer
 {
-    class CULLib_API ITimer
-    {
-    public:
-        ITimer();
-        virtual ~ITimer();
+public:
+    ITimer();
+    virtual ~ITimer();
 
-        virtual void start() = 0;
-        virtual void stop() = 0;
-        virtual void reset() = 0;
-        virtual const ITime& getElapsed()const = 0;
+    virtual void start() = 0;
+    virtual void stop() = 0;
+    virtual void reset() = 0;
+    virtual const ITime& getElapsed()const = 0;
 
-        static void sleepSeconds( const unsigned int seconds );
-        static void sleepMiliSeconds( const unsigned int mSeconds );
-        static void sleepMicroSeconds( const unsigned int uSeconds );
-    };
+    static void sleepSeconds( const unsigned int seconds );
+    static void sleepMiliSeconds( const unsigned int mSeconds );
+    static void sleepMicroSeconds( const unsigned int uSeconds );
+};
 
-    class CULLib_API TimerFactory
-    {
-    public:
-        static ITimer* getChronoTimer();
-    private:
-    };
-}
+class CULLib_API TimerFactory
+{
+public:
+    static ITimer* getChronoTimer();
+private:
+};
+
+NAMESPACE_END( CUL )
