761dbe6daf62b5bc6c4da22976943c2d85e793ed
Bartłomiej Kordek <bartekordek@gmail.com>
2019-12-04 18:48:50 +0100

    Add import for GLM.

---
+++ libcul/src/Math/IMPORT_glm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CUL/GenericUtils/DisableErros.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
