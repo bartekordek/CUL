8e3bff96e6ae7c7f9ab1e89b7915bb012b3b2a79
Bartłomiej Kordek <bartekordek@gmail.com>
2020-09-06 18:58:50 +0200

    Minor fixes.

--- libcul/inc/CUL/Graphics/IImage.hpp
+++ libcul/inc/CUL/Graphics/IImage.hpp
@@ -6,33 +6,47 @@
 NAMESPACE_BEGIN( CUL )
 NAMESPACE_BEGIN( Graphics )
 
-using DataType = unsigned int*;
+using DataType = unsigned char;
 using SSize2Dui = Graphics::SSize2Dui;
+using SSize2Di = Graphics::SSize2Di;
 using Path = FS::Path;
 
+enum class PixelFormat: short
+{
+    NONE = -1,
+    RGB24,
+    RGBA32
+};
+
+#if _MSC_VER
+#pragma warning( push )
+#pragma warning( disable: 4251 )
+#pragma warning( disable: 4820 )
+#endif
 struct CULLib_API ImageInfo
 {
     ImageInfo();
-    ImageInfo( const ImageInfo& arg );
-    ImageInfo( ImageInfo&& arg );
-    ImageInfo& operator=( const ImageInfo& rhv );
-    ImageInfo& operator=( ImageInfo&& rhv );
 
     Path path;
-    SSize2Dui size;
-    String colorFormat;
+    SSize2Di size = { 0, 0 };
+    String colorFormat = "";
+    PixelFormat pixelFormat = PixelFormat::NONE;
+    int depth = 0;
+    int pitch = 0;
 };
+#if _MSC_VER
+__pragma( warning( pop ) )
+#endif
 
 class CULLib_API IImage
 {
 public:
     explicit IImage();
 
-    virtual const DataType* getData() const = 0;
+    virtual DataType* getData() const = 0;
     virtual const ImageInfo& getImageInfo() const = 0;
 
     virtual ~IImage();
-
 protected:
 private:
 
