c85628636f47cba4007b389999c34b8c612594e5
Bartłomiej Kordek <bartekordek@gmail.com>
2020-09-18 14:13:36 +0200

    Additional fixes.

--- libcul/src/Graphics/ImageLoaderDevil.cpp
+++ libcul/src/Graphics/ImageLoaderDevil.cpp
@@ -5,7 +5,15 @@
 #include "CUL/Graphics/Color.hpp"
 #include "CUL/GenericUtils/SimpleAssert.hpp"
 #include "CUL/String.hpp"
+
 #include "Graphics/IMPORT_Devil.hpp"
+
+#include "CUL/STL_IMPORTS/STD_iostream.hpp"
+#include "CUL/STL_IMPORTS/STD_iosfwd.hpp"
+#include "CUL/STL_IMPORTS/STD_sstream.hpp"
+#include "CUL/STL_IMPORTS/STD_iomanip.hpp"
+
+#include "CUL/GenericUtils/Conversion.hpp"
 
 using namespace CUL::Graphics;
 
@@ -43,9 +51,55 @@ IImage* DevilImageLoader::loadImage( const Path& path, Cbool )
     const ILenum ilError = ilGetError();
     if( ilError != IL_NO_ERROR )
     {
+        std::stringstream stream;
+
+        std::string hexData( GUTILS::intToHex( ilError ) );
+
+
         auto errorIluAsString = iluErrorString( ilError );
-        CUL::Assert::simple( false, "ILU/ILUT cannot load image, error:" + String( errorIluAsString ) );
+        CUL::Assert::simple( false, "ILU/ILUT cannot load image, error:" + String( errorIluAsString ) + ", hex = " + hexData );
     }
+
+    const auto origin = ilGetInteger( IL_IMAGE_ORIGIN );
+    ILint format = ilGetInteger( IL_IMAGE_FORMAT );
+
+    if( format == IL_COLOUR_INDEX )
+    {
+
+    }
+    else if( format == IL_BGR )
+    {
+
+    }
+    else if( format == IL_BGRA )
+    {
+
+    }
+
+    ILint orientation = ilGetInteger( IL_IMAGE_ORIGIN );
+
+    if( orientation == 1 )
+    {
+
+    }
+
+    std::cout << "orientation = " << orientation << "\n";
+
+
+
+    ILenum placement = ILU_UPPER_LEFT;
+
+    if( IL_ORIGIN_LOWER_LEFT == origin || format )
+    {
+        placement = ILU_LOWER_LEFT;
+    }
+    else if( IL_ORIGIN_UPPER_LEFT == origin )
+    {
+        placement = ILU_UPPER_LEFT;
+    }
+
+    
+
 
     // TODO: Is it really necessary?
     success = ilConvertImage( IL_RGBA, IL_UNSIGNED_BYTE );
@@ -58,7 +112,11 @@ IImage* DevilImageLoader::loadImage( const Path& path, Cbool )
     ii.depth = 24;//static_cast<int>( ilGetInteger( IL_IMAGE_DEPTH ) );// 24
 
 
-    iluImageParameter( ILU_PLACEMENT, ILU_UPPER_LEFT );
+
+
+    
+
+    //iluImageParameter( ILU_PLACEMENT, ILU_UPPER_LEFT );
 
     //Resize image
     iluEnlargeCanvas(
@@ -66,13 +124,15 @@ IImage* DevilImageLoader::loadImage( const Path& path, Cbool )
         static_cast<ILuint>( imgHeight ),
         1 );
 
+    iluImageParameter( ILU_PLACEMENT, IL_ORIGIN_LOWER_LEFT );
+
     const auto imageConcrete = new ImageConcrete();
 
     ii.size.width = static_cast<int>( imgWidth );
     ii.size.height = static_cast<int>( imgHeight );
     
     ii.pitch = 4 * static_cast<int>( imgWidth ); //1628
-    ii.pixelFormat = PixelFormat::RGBA32; //376840196
+    ii.pixelFormat = PixelFormat::RGBA; //376840196
     ii.path = path;
 
 
@@ -89,7 +149,6 @@ IImage* DevilImageLoader::loadImage( const Path& path, Cbool )
     imageConcrete->setData( dataCopy );
     imageConcrete->setPath( path );
     imageConcrete->setImageInfo( ii );
-    //imageConcrete->disableRelease( true );
 
     m_fileList[path.getPath()] = std::unique_ptr<IImage>( imageConcrete );
 
