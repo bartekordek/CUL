63645e4ef6471d795d0073ce28cadda9b28e2d37
Bartłomiej kordek <bartekordek@gmail.com>
2022-03-10 13:22:55 +0100

    Add cppfs.

---
+++ libcul/src/Filesystem/IMPORT_fscpp.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CUL/GenericUtils/DisableErros.hpp"
+
+#include <cppfs/fs.h>
+#include <cppfs/FileHandle.h>
+#include <cppfs/FileIterator.h>
+#include <cppfs/FilePath.h>
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
