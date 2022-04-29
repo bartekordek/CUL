fb282b994b8d50e3a1e5dcc7bd79d859e24ee72b
Bartłomiej Kordek <bartekordek@gmail.com>
2022-04-29 16:30:39 +0200

    Add version header stub.

---
+++ libcul/inc/CUL/GenericUtils/Version.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CUL/CUL.hpp"
+
+NAMESPACE_BEGIN( CUL )
+NAMESPACE_BEGIN( GUTILS )
+
+struct CULLib_API Version
+{
+public:
+    int major = 0;
+    int minor = 0;
+protected:
+private:
+};
+
+NAMESPACE_END( GUTILS )
+NAMESPACE_END( CUL )
