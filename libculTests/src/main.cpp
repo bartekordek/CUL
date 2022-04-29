9a937a574eef6e6ea5ce18fa5590c063cb0c5814
Bartłomiej Kordek <bartekordek@gmail.com>
2022-04-29 11:50:25 +0200

    Add debug print.

--- libculTests/src/main.cpp
+++ libculTests/src/main.cpp
@@ -1,7 +1,11 @@
 #include "Gtest.hpp"
 
+#include "CUL/STL_IMPORTS/STD_iostream.hpp"
+
 int main( int argc, char** argv )
 {
+    std::cout << "Begin tests.\n";
+
     ::testing::InitGoogleTest( &argc, argv );
     const int result = RUN_ALL_TESTS();
     return result;
