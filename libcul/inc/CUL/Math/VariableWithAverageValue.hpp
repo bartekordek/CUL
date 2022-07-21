f10a1a6cb9a7e05a34206464660e3e6310e7fe88
Bartłomiej kordek <bartekordek@gmail.com>
2022-07-21 16:21:03 +0200

    Add small class for average calculation.

---
+++ libcul/inc/CUL/Math/VariableWithAverageValue.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CUL/GenericUtils/DumbPtr.hpp"
+
+#include "CUL/STL_IMPORTS/STD_memory_resource.hpp"
+
+
+NAMESPACE_BEGIN( CUL )
+NAMESPACE_BEGIN( MATH )
+
+constexpr size_t G_arraySize = 1024;
+
+class CULLib_API VariableWithAverageValue final
+{
+public:
+    VariableWithAverageValue();
+
+    void add( float value );
+    float getAverage();
+
+    size_t getcapacity() const;
+    void setCapacity( size_t newCapacity );
+
+    ~VariableWithAverageValue();
+protected:
+private:
+    void calculateAverage();
+    size_t m_sampleCapacity = 4;
+
+    std::array<std::byte, G_arraySize> m_bufferBlocks;
+    std::pmr::monotonic_buffer_resource m_buffer_src;
+    std::pmr::vector<float> m_samples;
+
+    float m_lastVal = 0.f;
+    float m_average = 0.f;
+
+    VariableWithAverageValue( const VariableWithAverageValue& arg ) = delete;
+    VariableWithAverageValue( VariableWithAverageValue&& arg ) = delete;
+    VariableWithAverageValue& operator=( const VariableWithAverageValue& arg ) = delete;
+    VariableWithAverageValue& operator=( VariableWithAverageValue&& arg ) = delete;
+};
+
+NAMESPACE_END( MATH )
+NAMESPACE_END( CUL )
---
+++ libcul/src/Math/VariableWithAverageValue..cpp
@@ -0,0 +1,54 @@
+#include "CUL/Math/VariableWithAverageValue.hpp"
+
+#include "CUL/IMPORT_boost_circular_buffer.hpp"
+
+using namespace CUL;
+using namespace MATH;
+
+VariableWithAverageValue::VariableWithAverageValue():
+    m_buffer_src( &m_bufferBlocks, sizeof( m_bufferBlocks ) ), 
+    m_samples( &m_buffer_src )
+{
+}
+
+void VariableWithAverageValue::add( float value )
+{
+    m_samples.push_back( value );
+    m_lastVal = value;
+    if( m_samples.size() > m_sampleCapacity )
+    {
+        m_samples.erase(m_samples.begin());
+    }
+    calculateAverage();
+}
+
+float VariableWithAverageValue::getAverage()
+{
+    return m_average;
+}
+
+size_t VariableWithAverageValue::getcapacity() const
+{
+    return m_sampleCapacity;
+}
+
+void VariableWithAverageValue::setCapacity( size_t newCapacity )
+{
+    m_sampleCapacity = newCapacity;
+}
+
+void VariableWithAverageValue::calculateAverage()
+{
+    m_average = 0.f;
+    size_t currentSize = m_samples.size();
+    for( size_t i = 0; i < currentSize; ++i )
+    {
+        m_average += m_samples[i];
+    }
+
+    m_average /= (float)currentSize;
+}
+
+VariableWithAverageValue::~VariableWithAverageValue()
+{
+}
