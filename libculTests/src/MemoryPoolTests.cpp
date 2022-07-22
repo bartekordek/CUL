3d674dd48a0ebe6b2faaaabbcb5029c3ef5c566f
Bartłomiej kordek <bartekordek@gmail.com>
2022-07-23 00:11:34 +0200

    Test avg value.

--- libculTests/src/MemoryPoolTests.cpp
+++ libculTests/src/MemoryPoolTests.cpp
@@ -3,6 +3,8 @@
 #include "CUL/Memory/MemoryPool.hpp"
 #include "CUL/GenericUtils/DumbPtr.hpp"
 
+#include "CUL/Math//VariableWithAverageValue.hpp"
+
 #include "CUL/STL_IMPORTS/STD_iostream.hpp"
 
 using namespace CUL;
@@ -20,4 +22,30 @@ TEST_F( MemoryPoolTests, TEST_SINGLE_ALLOCATION )
 TEST_F( MemoryPoolTests, TEST_MULTIPLE_ALLOCATION )
 {
     //TODO:
+}
+
+TEST_F( MemoryPoolTests, TESTING_0 )
+{
+    MATH::VariableWithAverageValue average;
+    average.add( 1.0 );
+    average.add( 2.0 );
+    average.add( 3.0 );
+
+    float averageVal = average.getAverage();
+    int averageInt = (int)averageVal;
+    ASSERT_EQ( 2, averageInt );
+}
+
+TEST_F( MemoryPoolTests, TESTING_1 )
+{
+    MATH::VariableWithAverageValue average;
+    average.add( 1.0 );
+    average.add( 4.0 );
+    average.add( 4.0 );
+    average.add( 4.0 );
+    average.add( 4.0 );
+
+    float averageVal = average.getAverage();
+    int averageInt = (int)averageVal;
+    ASSERT_EQ( 4, averageInt );
 }
