b0dc4eccd819e5bcbbe1a5c34affa843d755edd8
Bartłomiej kordek <bartekordek@gmail.com>
2022-07-22 14:32:21 +0200

    Update FPS counter with timers.

--- libcul/src/Video/FPSCounterConcrete.cpp
+++ libcul/src/Video/FPSCounterConcrete.cpp
@@ -7,16 +7,27 @@ using namespace Video;
 
 FPSCounterConcrete::FPSCounterConcrete()
 {
-    m_samples.set_capacity( bufferSize );
-    m_timer.reset( CUL::TimerFactory::getChronoTimer() );
-}
-
-FPSCounterConcrete::~FPSCounterConcrete()
-{
-    if( m_isRunning )
+    auto print = []( const boost::circular_buffer<int>& buff )
     {
-        stop();
-    }
+        size_t size = buff.size();
+        std::cout << "\nbuff: \n";
+        for( size_t i = 0; i < size; ++i )
+        {
+            std::cout << buff[i] << "\n";
+        }
+    };
+
+    boost::circular_buffer<int> dupa( 3 );
+
+    dupa.push_back( 1 );
+    dupa.push_back( 2 );
+    dupa.push_back( 3 );
+    print( dupa );
+    dupa.push_back( 4 );
+    print( dupa );
+
+    m_samples.set_capacity( m_bufferSize );
+    m_timer.reset( CUL::TimerFactory::getChronoTimer() );
 }
 
 void FPSCounterConcrete::start()
@@ -24,6 +35,7 @@ void FPSCounterConcrete::start()
     if( false == m_isRunning )
     {
         m_isRunning = true;
+        m_timer->start();
         m_mainThread = std::thread( &FPSCounterConcrete::counterLoop, this );
     }
 }
@@ -39,7 +51,12 @@ void FPSCounterConcrete::stop()
 
 void FPSCounterConcrete::increase()
 {
-    m_framesCount = m_framesCount + 1.f;
+    m_timer->stop();
+    m_lastFrameDuration = m_timer->getElapsed().getMs();
+    m_lastFrameValue = 1000.f / m_lastFrameDuration;
+    m_samples.push_back( m_lastFrameValue );
+    m_averageFps = calculateAverageFps();
+    m_timer->start();
 }
 
 double FPSCounterConcrete::getCurrentFps() const
@@ -55,6 +72,7 @@ double FPSCounterConcrete::getAverageFps() const
 void FPSCounterConcrete::setSampleSize( unsigned short sampleSize )
 {
     m_samples.resize( sampleSize );
+    m_bufferSize = sampleSize;
 }
 
 void FPSCounterConcrete::setMeasurePeriod( const unsigned periodMs )
@@ -64,13 +82,20 @@ void FPSCounterConcrete::setMeasurePeriod( const unsigned periodMs )
 
 void FPSCounterConcrete::counterLoop()
 {
-    while( m_isRunning )
+    //while( m_isRunning )
+    //{
+    //    m_timer->sleepMiliSeconds( m_sleepTimeMs );
+    //    m_samples.push_front( m_lastFrameValue );
+    //    m_averageFps = calculateAverageFps();
+    //}
+}
+
+
+FPSCounterConcrete::~FPSCounterConcrete()
+{
+    if( m_isRunning )
     {
-        m_timer->sleepMiliSeconds( m_sleepTimeMs );
-        m_lastFrameValue.store( m_framesCount );
-        m_samples.push_front( m_lastFrameValue );
-        m_averageFps = calculateAverageFps();
-        m_framesCount = 0.f;
+        stop();
     }
 }
 
@@ -82,13 +107,13 @@ double FPSCounterConcrete::calculateAverageFps() const
     }
 
     double sum = 0.0;
-    for( auto sampleVal: m_samples )
+    for( size_t i = 0; i < m_bufferSize; ++i )
     {
-        sum += sampleVal;
+        sum += m_samples[i];
     }
 
-    const auto result = sum / (double)m_samples.size();
-    return normalizeToSleepTime( result );
+    sum /= ( static_cast<decltype( sum )>( m_bufferSize ) );
+    return sum;
 }
 
 double FPSCounterConcrete::normalizeToSleepTime( const double fpsCount ) const
--- libcul/src/Video/FPSCounterConcrete.hpp
+++ libcul/src/Video/FPSCounterConcrete.hpp
@@ -39,14 +39,14 @@ private:
     void counterLoop();
     double calculateAverageFps() const;
     double normalizeToSleepTime( const double fpsCount ) const;
-    unsigned int bufferSize = 16;
+    size_t m_bufferSize = 16;
 
     std::thread m_mainThread;
     std::unique_ptr<CUL::ITimer> m_timer;
     std::atomic<bool> m_isRunning = { false };//TODO: delegate thread
     //creation to separate class, to check if thread was created and is runing.
-    std::atomic<float> m_framesCount = { 0.f };
     std::atomic<float> m_lastFrameValue = { 0.f };
+    std::atomic<float> m_lastFrameDuration = { 0.f };
     boost::circular_buffer<float> m_samples;
     std::atomic<double> m_averageFps = { 0.0 };
     std::atomic<unsigned> m_sleepTimeMs = { 1000 };
