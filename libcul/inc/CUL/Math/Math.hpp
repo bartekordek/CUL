6a5562fb4efdfc39f38c55656aa51ecacf8320dd
Bartłomiej kordek <bartekordek@gmail.com>
2022-12-14 15:24:56 +0100

    I hate you, msvc.

--- libcul/inc/CUL/Math/Math.hpp
+++ libcul/inc/CUL/Math/Math.hpp
@@ -72,7 +72,14 @@ Type lerp(Type a, Type b, Type f)
 #endif
 
 NAMESPACE_BEGIN( Constants )
+#ifdef _MSC_VER
+#pragma warning( push, 0 )
+#pragma warning( disable: 5264 )
+#endif
 static constexpr double PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481;
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
 NAMESPACE_END( Constants )
 
 NAMESPACE_END( UTIL )
