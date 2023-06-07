0ef26fa12211e5d95333639316ae83627609c99d
Bartłomiej Kordek <bartekordek@gmail.com>
2023-06-07 20:33:33 +0200

    Triangle implementation is now more like a quad.

--- libcul/inc/CUL/Math/Primitives/Triangle.hpp
+++ libcul/inc/CUL/Math/Primitives/Triangle.hpp
@@ -7,45 +7,44 @@ NAMESPACE_BEGIN( CUL )
 NAMESPACE_BEGIN( MATH )
 NAMESPACE_BEGIN( Primitives )
 
-#if _MSC_VER
-#pragma warning( push )
-#pragma warning( disable: 4514 )
-#endif
-
-class CULLib_API Triangle final:
-    public ISerializable
+class CULLib_API Triangle final: public ISerializable
 {
 public:
-    using Type = float;
-    using PointType = Vector3D<Type>;
-    using ValuesType = std::array< PointType, 3 >;
-
-    ValuesType vals;
+    using PointType = std::array<float, 6>;
+    using QuadData = std::array<PointType, 3>;
 
     Triangle();
     Triangle( const Triangle& arg );
+    Triangle( const QuadData& dataIn );
 
     Triangle& operator=( const Triangle& arg );
-    PointType& operator[]( size_t index );
+    Triangle& operator=( const QuadData& dataIn );
 
+    void* dataVoid();
+
+    void setData( const QuadData& dataIn );
+
+    PointType& operator[]( size_t index );
     const PointType& operator[]( size_t index ) const;
 
+    QuadData data;
+    void* dataAsVoid = nullptr;
     std::vector<float> toVectorOfFloat() const;
+
+    static size_t getStride();
 
     ~Triangle();
 
 protected:
 private:
+    void initializeData();
+    void updateData();
     String getSerializationContent( CounterType tabsSize, const bool = false ) const override;
 
     // Deleted:
     Triangle( Triangle&& arg ) = delete;
     Triangle& operator=( Triangle&& arg ) = delete;
 };
-
-#ifdef _MSC_VER
-#pragma warning( pop )
-#endif
 
 NAMESPACE_END( Primitives )
 NAMESPACE_END( MATH )
--- libcul/src/Math/Primitives/Triangle.cpp
+++ libcul/src/Math/Primitives/Triangle.cpp
@@ -1,77 +1,119 @@
 #include "CUL/Math/Primitives/Triangle.hpp"
 
-NAMESPACE_BEGIN( CUL )
-NAMESPACE_BEGIN( MATH )
-NAMESPACE_BEGIN( Primitives )
+using namespace CUL;
+using namespace MATH;
+using namespace Primitives;
 
 Triangle::Triangle()
 {
+    initializeData();
+    updateData();
 }
 
-Triangle::Triangle( const Triangle& arg ) : ISerializable(), vals( arg.vals )
+Triangle::Triangle( const Triangle& arg ) : ISerializable(), data( arg.data )
 {
+    initializeData();
+    updateData();
+}
+
+Triangle::Triangle( const QuadData& dataIn )
+{
+    setData( dataIn );
+}
+
+void Triangle::initializeData()
+{
+    for( auto& point : data )
+    {
+        for( auto& value : point )
+        {
+            value = 0.f;
+        }
+    }
 }
 
 Triangle& Triangle::operator=( const Triangle& arg )
 {
     if( this != &arg )
     {
-        vals = arg.vals;
+        data = arg.data;
+        updateData();
     }
     return *this;
 }
 
+Triangle& Triangle::operator=( const QuadData& dataIn )
+{
+    setData( dataIn );
+    return *this;
+}
+
+void* Triangle::dataVoid()
+{
+    return dataAsVoid;
+}
+
+void Triangle::setData( const QuadData& dataIn )
+{
+    data = dataIn;
+}
+
 Triangle::PointType& Triangle::operator[]( size_t index )
 {
-    return vals[index];
+    return data[index];
 }
 
 const Triangle::PointType& Triangle::operator[]( size_t index ) const
 {
-    return vals[index];
+    return data[index];
 }
 
-std::vector<float> Triangle::toVectorOfFloat() const
+void Triangle::updateData()
 {
-    std::vector<float> result;
-
-    for( size_t i = 0; i < 3; ++i )
-    {
-        result.push_back( vals[i].getX() );
-        result.push_back( vals[i].getY() );
-        result.push_back( vals[i].getZ() );
-    }
-
-    return result;
+    auto convertedToVoid = reinterpret_cast<const void*>( &data );
+    dataAsVoid = const_cast<void*>( convertedToVoid );
 }
 
 String Triangle::getSerializationContent( CounterType tabsSize, const bool ) const
 {
     String tabs = getTab( tabsSize );
 
-    auto convertPoint = []( const PointType& value ){
-        return String( "{ " ) +
-            String( value.getX() ) +
-            String( ", " ) +
-            String( value.getY() ) +
-            String( ", " ) +
-            String( value.getZ() ) +
-            String( " }" );
+    auto convertPoint = []( const PointType& value )
+    {
+        return String( "{ " ) + String( value[0] ) + String( ", " ) + String( value[1] ) + String( ", " ) + String( value[2] ) +
+               String( " }" );
     };
 
     String result;
     result = result + tabs + "    \"name\":\"Triangle\",\n";
-    result = result + tabs + "    \"p1\": " + convertPoint( vals[0] ) + ",\n";
-    result = result + tabs + "    \"p2\": " + convertPoint( vals[1] ) + ",\n";
-    result = result + tabs + "    \"p3\": " + convertPoint( vals[2] ) + "\n";
+    result = result + tabs + "    \"p1\": " + convertPoint( data[0] ) + ",\n";
+    result = result + tabs + "    \"p2\": " + convertPoint( data[1] ) + ",\n";
+    result = result + tabs + "    \"p3\": " + convertPoint( data[2] ) + ",\n";
+    result = result + tabs + "    \"p4\": " + convertPoint( data[3] ) + "\n";
 
     return result;
+}
+
+std::vector<float> Triangle::toVectorOfFloat() const
+{
+    std::vector<float> result;
+
+    for( const auto& row : data )
+    {
+        for( const auto& cell : row )
+        {
+            result.push_back( cell );
+        }
+    }
+
+    return result;
+}
+
+size_t Triangle::getStride()
+{
+    return sizeof( PointType );
 }
 
 Triangle::~Triangle()
 {
 }
-
-NAMESPACE_END( Primitives )
-NAMESPACE_END( MATH )
-NAMESPACE_END( CUL )
