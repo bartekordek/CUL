c76d19f6ba16a1fcf18c441ac96c8b22922f97ac
Bartłomiej Kordek <bartekordek@gmail.com>
2023-10-17 17:50:19 +0200

    Add simple ObjectRegistry.

---
+++ libcul/src/ObjectRegistry.cpp
@@ -0,0 +1,39 @@
+#include "CUL/ObjectRegistry.hpp"
+
+CUL::ObjectRegistry::ObjectRegistry()
+{
+}
+
+
+std::uint64_t CUL::ObjectRegistry::getFirstFreeIndex()
+{
+    std::uint64_t result = 0u;
+    std::lock_guard<std::mutex> locker( m_listMtx );
+
+    for( std::uint64_t i = 0; i < std::numeric_limits<std::uint64_t>::max(); ++i )
+    {
+        const auto it = m_list.find(i);
+        if( it == m_list.end() )
+        {
+            result = i;
+            m_list.insert( i );
+            break;
+        }
+    }
+
+    return result;
+}
+
+void CUL::ObjectRegistry::removeIndex( std::uint64_t index )
+{
+    std::lock_guard<std::mutex> locker( m_listMtx );
+    const auto it = m_list.find( index );
+    if( it != m_list.end() )
+    {
+        m_list.erase( it );
+    }
+}
+
+CUL::ObjectRegistry::~ObjectRegistry()
+{
+}
