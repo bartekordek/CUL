3e8f1e7917541c2e0fd1b239aae8944582d14f09
Bartłomiej Kordek <bartekordek@gmail.com>
2025-01-08 00:29:33 +0100

    Add data wrapper class.

---
+++ libcul/inc/CUL/Memory/DataWrapper.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CUL/GenericUtils/Types.hpp"
+
+#include "CUL/STL_IMPORTS/STD_stddef.hpp"
+#include "CUL/STL_IMPORTS/STD_vector.hpp"
+#include "CUL/STL_IMPORTS/STD_cstdint.hpp"
+#include "CUL/STL_IMPORTS/STD_string.hpp"
+
+NAMESPACE_BEGIN( CUL )
+
+class CULLib_API DataWrapper final
+{
+public:
+    DataWrapper();
+    DataWrapper( const DataWrapper& dw );
+    DataWrapper( DataWrapper&& dw );
+    DataWrapper& operator=( const DataWrapper& dw );
+    DataWrapper& operator=( DataWrapper&& dw );
+
+    void load( const void* data, std::size_t size, ETypes inType );
+    void* getData();
+    const void* getData() const;
+    std::size_t getSize() const;
+
+    void createFrom( const std::vector<float>& inData );
+    void createFrom( const std::vector<std::uint32_t>& inData );
+
+    void setType( ETypes inType );
+    ETypes getType() const;
+
+    bool getIsEmpty() const;
+
+    ~DataWrapper();
+
+protected:
+private:
+    void release();
+    std::vector<std::uint8_t> m_value;
+    ETypes m_type{ ETypes::Unkown };
+};
+
+NAMESPACE_END( CUL )
---
+++ libcul/src/Memory/DataWrapper.cpp
@@ -0,0 +1,111 @@
+#include "CUL/Memory/DataWrapper.hpp"
+#include "CUL/GenericUtils/SimpleAssert.hpp"
+#include "CUL/CULInterface.hpp"
+#include "CUL/Threading/ThreadUtil.hpp"
+
+#include "CUL/STL_IMPORTS/STD_memory.hpp"
+#include "CUL/STL_IMPORTS/STD_cstring.hpp"
+
+using namespace CUL;
+
+DataWrapper::DataWrapper()
+{
+
+}
+
+DataWrapper::DataWrapper( const DataWrapper& dw ):
+    m_value( dw.m_value ),
+    m_type( dw.m_type )
+{
+}
+
+DataWrapper::DataWrapper( DataWrapper&& dw ):
+    m_value( dw.m_value ),
+    m_type( dw.m_type )
+{
+    dw.m_value.clear();
+    dw.m_type = ETypes::Unkown;
+}
+
+DataWrapper& DataWrapper::operator=( const DataWrapper& dw )
+{
+    if( this != &dw )
+    {
+        m_value = dw.m_value;
+        m_type = dw.m_type;
+    }
+    return *this;
+}
+
+DataWrapper& DataWrapper::operator=( DataWrapper&& dw )
+{
+    if( this != &dw )
+    {
+        m_value = dw.m_value;
+        m_type = dw.m_type;
+        dw.m_value.clear();
+        dw.m_type = ETypes::Unkown;
+    }
+    return *this;
+}
+
+void DataWrapper::createFrom( const std::vector<float>& inData )
+{
+    const std::uint64_t dataSize = sizeof( float ) * inData.size();
+    load( static_cast<const void*>( inData.data() ), dataSize, CUL::ETypes::Float );
+}
+
+void DataWrapper::createFrom( const std::vector<std::uint32_t>& inData )
+{
+    const std::uint64_t dataSize = sizeof( std::uint32_t ) * inData.size();
+    load( static_cast<const void*>( inData.data() ), dataSize, CUL::ETypes::Uint32 );
+}
+
+void DataWrapper::setType( ETypes inType )
+{
+    m_type = inType;
+}
+
+ETypes DataWrapper::getType() const
+{
+    return m_type;
+}
+
+void DataWrapper::load( const void* data, std::size_t size, ETypes inType )
+{
+    release();
+    m_value.resize( size );
+    std::memcpy( m_value.data(), data, size );
+    m_type = inType;
+    CUL::Assert::simple( m_type != ETypes::Unkown, "NOT IN THE RENDER THREAD." );
+}
+
+bool DataWrapper::getIsEmpty() const
+{
+    return m_value.empty();
+}
+
+void* DataWrapper::getData()
+{
+    return m_value.data();
+}
+
+const void* DataWrapper::getData() const
+{
+    return m_value.data();
+}
+
+std::size_t DataWrapper::getSize() const
+{
+    return m_value.size();
+}
+
+DataWrapper::~DataWrapper()
+{
+    release();
+}
+
+void DataWrapper::release()
+{
+    m_value.clear();
+}
