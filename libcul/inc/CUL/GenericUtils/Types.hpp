0abf83c1e1dad1ca747cc7fc382c841d17fbd9ff
Bartłomiej Kordek <bartekordek@gmail.com>
2025-01-08 00:28:35 +0100

    Add types header.

---
+++ libcul/inc/CUL/GenericUtils/Types.hpp
@@ -0,0 +1,22 @@
+#include "CUL/CUL.hpp"
+
+#include "CUL/STL_IMPORTS/STD_cstdint.hpp"
+
+NAMESPACE_BEGIN( CUL )
+
+enum class ETypes: std::uint16_t
+{
+    Unkown = 0,
+    Float,
+    Double,
+    Uint8,
+    Int8,
+    Uint16,
+    Int16,
+    Uint32,
+    Int32,
+    Uint64,
+    Int64
+};
+
+NAMESPACE_END( CUL )
