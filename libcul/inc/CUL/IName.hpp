ecb06748208371d092a0971ba3db53a9c1da07ce
Bartłomiej Kordek <bartekordek@gmail.com>
2025-03-01 10:14:37 +0100

    Replace callback with inheritance.

--- libcul/inc/CUL/IName.hpp
+++ libcul/inc/CUL/IName.hpp
@@ -3,8 +3,6 @@
 #include "CUL.hpp"
 #include "CUL/String.hpp"
 
-#include "CUL/STL_IMPORTS/STD_functional.hpp"
-
 NAMESPACE_BEGIN( CUL )
 
 class CULLib_API IName
@@ -20,11 +18,11 @@ public:
     const String& getName() const;
     virtual void setName( const String& name );
 
-    std::function<void( const String& newName )> AfterNameChangeCallback;
-
     virtual ~IName();
 
 protected:
+    virtual void onNameChange( const String& newName );
+
 private:
     String m_name;
 };
--- libcul/src/IName.cpp
+++ libcul/src/IName.cpp
@@ -2,28 +2,26 @@
 
 using namespace CUL;
 
-CUL::IName::IName()
+IName::IName()
 {
 }
 
-CUL::IName::IName( const String& name ):
+IName::IName( const String& name ):
     m_name( name )
 {
 }
 
-CUL::IName::IName( const IName& val ) : m_name( val.m_name )
+IName::IName( const IName& val ):
+    m_name( val.m_name )
 {
 }
 
-CUL::IName::IName( IName&& val ) noexcept : m_name( val.m_name )
+IName::IName( IName&& val ) noexcept :
+    m_name( val.m_name )
 {
 }
 
-CUL::IName::~IName()
-{
-}
-
-IName& CUL::IName::operator=( const IName& rhv )
+IName& IName::operator=( IName&& rhv ) noexcept
 {
     if( &rhv != this )
     {
@@ -32,25 +30,34 @@ IName& CUL::IName::operator=( const IName& rhv )
     return *this;
 }
 
-IName& CUL::IName::operator=( IName&& rhv ) noexcept
-{
-    if( &rhv != this )
-    {
-        m_name = rhv.m_name;
-    }
-    return *this;
-}
-
-const String& CUL::IName::getName() const
+const String& IName::getName() const
 {
     return m_name;
 }
 
-void CUL::IName::setName( const String& name )
+IName& IName::operator=( const IName& rhv )
+{
+    if( &rhv != this )
+    {
+        m_name = rhv.m_name;
+    }
+    return *this;
+}
+
+void IName::setName( const String& name )
 {
     m_name = name;
-    if( AfterNameChangeCallback )
-    {
-        AfterNameChangeCallback( name );
-    }
+    onNameChange( m_name );
 }
+
+void IName::onNameChange( const String& )
+{
+}
+
+IName::~IName()
+{
+}
+
+
+
+
