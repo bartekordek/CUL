50c3dcd45fcaef12e554ee113fc4dab3ae0b2fa6
Bartłomiej Kordek <bartekordek@gmail.com>
2025-06-10 10:56:31 +0200

    Added macro helper to mark class as noncopyable.

--- libcul/inc/CUL/UselessMacros.hpp
+++ libcul/inc/CUL/UselessMacros.hpp
@@ -2,3 +2,10 @@
 
 #define NAMESPACE_BEGIN( name ) namespace name {
 #define NAMESPACE_END( name ) }// name
+
+
+#define CUL_NONCOPYABLE( TypeName )                   \
+TypeName( TypeName&& ) = delete;                 \
+TypeName( const TypeName& ) = delete;            \
+TypeName& operator=( const TypeName& ) = delete; \
+TypeName& operator=( TypeName&& ) = delete;
