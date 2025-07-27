cb09fe4423c4ca72e1e8f397ef49c00be603abc3
Bartłomiej Kordek <bartekordek@gmail.com>
2025-07-27 22:38:00 +0200

    Some fixes for average accumulator.

--- libcul/inc/CUL/Math/VariableWithAverageValue.hpp
+++ libcul/inc/CUL/Math/VariableWithAverageValue.hpp
@@ -3,7 +3,7 @@
 #include "CUL/GenericUtils/DumbPtr.hpp"
 
 #include "CUL/STL_IMPORTS/STD_array.hpp"
-#include "CUL/STL_IMPORTS/STD_memory_resource.hpp"
+#include "CUL/STL_IMPORTS/STD_vector.hpp"
 
 
 NAMESPACE_BEGIN( CUL )
@@ -16,10 +16,17 @@ class CULLib_API VariableWithAverageValue final
 public:
     VariableWithAverageValue();
 
+    VariableWithAverageValue( const VariableWithAverageValue& arg );
+    VariableWithAverageValue( VariableWithAverageValue&& arg );
+    VariableWithAverageValue& operator=( const VariableWithAverageValue& arg );
+    VariableWithAverageValue& operator=( VariableWithAverageValue&& arg ) noexcept;
+
     void add( float value );
     float getAverage() const;
+    float getMax() const;
+    float getMin() const;
 
-    size_t size() const;
+    std::size_t size() const;
     void setCapacity( size_t newCapacity );
 
     ~VariableWithAverageValue();
@@ -28,19 +35,14 @@ private:
     void calculateAverage();
     size_t m_sampleCapacity = 4;
 
-    std::array<std::byte, G_arraySize> m_bufferBlocks;
-    std::pmr::monotonic_buffer_resource m_buffer_src;
-    std::pmr::vector<float> m_samples;
+    std::vector<float> m_samples;
 
     float m_lastVal = 0.f;
     float m_average = 0.f;
-
-    VariableWithAverageValue( const VariableWithAverageValue& arg ) = delete;
-    VariableWithAverageValue( VariableWithAverageValue&& arg ) = delete;
-    VariableWithAverageValue& operator=( const VariableWithAverageValue& arg ) = delete;
-    VariableWithAverageValue& operator=( VariableWithAverageValue&& arg ) = delete;
+    float m_maxValue{ std::numeric_limits<float>::min() };
+    float m_minValue{ std::numeric_limits<float>::max() };
 };
-
+using VWAV = VariableWithAverageValue;
 
 NAMESPACE_END( MATH )
 NAMESPACE_END( CUL )
--- libcul/src/Math/VariableWithAverageValue..cpp
+++ libcul/src/Math/VariableWithAverageValue..cpp
@@ -3,14 +3,73 @@
 using namespace CUL;
 using namespace MATH;
 
-VariableWithAverageValue::VariableWithAverageValue():
-    m_buffer_src( &m_bufferBlocks, sizeof( m_bufferBlocks ) ), 
-    m_samples( &m_buffer_src )
+VariableWithAverageValue::VariableWithAverageValue()
 {
+}
+
+VariableWithAverageValue::VariableWithAverageValue( const VariableWithAverageValue& arg )
+    : m_sampleCapacity( arg.m_sampleCapacity ),
+      m_samples( arg.m_samples ),
+      m_lastVal( arg.m_lastVal ),
+      m_average( arg.m_average ),
+      m_maxValue(arg.m_maxValue),
+      m_minValue(arg.m_minValue)
+{
+}
+
+VariableWithAverageValue::VariableWithAverageValue( VariableWithAverageValue&& arg )
+    : m_sampleCapacity( arg.m_sampleCapacity ),
+      m_samples( std::move( arg.m_samples ) ),
+      m_lastVal( arg.m_lastVal ),
+      m_average( arg.m_average ),
+      m_maxValue( arg.m_maxValue ),
+      m_minValue( arg.m_minValue )
+{
+    arg.m_sampleCapacity = 0;
+}
+
+VariableWithAverageValue& VariableWithAverageValue::operator=( const VariableWithAverageValue& arg )
+{
+    if( this != &arg )
+    {
+        m_sampleCapacity = arg.m_sampleCapacity;
+        m_samples = arg.m_samples;
+        m_lastVal = arg.m_lastVal;
+        m_average = arg.m_average;
+        m_maxValue = arg.m_maxValue;
+        m_minValue = arg.m_minValue;
+    }
+    return *this;
+}
+
+VariableWithAverageValue& VariableWithAverageValue::operator=( VariableWithAverageValue&& arg ) noexcept
+{
+    if( this != &arg )
+    {
+        m_sampleCapacity = arg.m_sampleCapacity;
+        m_samples = std::move( arg.m_samples );
+        m_lastVal = arg.m_lastVal;
+        m_average = arg.m_average;
+        m_maxValue = arg.m_maxValue;
+        m_minValue = arg.m_minValue;
+
+        arg.m_sampleCapacity = 0u;
+        arg.m_lastVal = 0.f;
+        m_average = 0.f;
+        m_maxValue = 0.f;
+        arg.m_minValue = 0.f;
+
+    }
+    return *this;
 }
 
 void VariableWithAverageValue::add( float value )
 {
+    if( std::isnan( value ) )
+    {
+        auto x = 0;
+    }
+
     m_samples.push_back( value );
     m_lastVal = value;
     if( m_samples.size() > m_sampleCapacity )
@@ -23,6 +82,16 @@ void VariableWithAverageValue::add( float value )
 float VariableWithAverageValue::getAverage() const
 {
     return m_average;
+}
+
+float VariableWithAverageValue::getMax() const
+{
+    return m_maxValue;
+}
+
+float VariableWithAverageValue::getMin() const
+{
+    return m_minValue;
 }
 
 size_t VariableWithAverageValue::size() const
@@ -39,9 +108,29 @@ void VariableWithAverageValue::calculateAverage()
 {
     m_average = 0.f;
     size_t currentSize = m_samples.size();
-    for( size_t i = 0; i < currentSize; ++i )
+
+    if( currentSize > 0 )
     {
-        m_average += m_samples[i];
+        //m_minValue = std::min( m_samples[0u], m_minValue );
+        //m_maxValue = std::max( m_samples[0u], m_maxValue );
+
+        m_minValue = m_samples[0u];
+        m_maxValue = m_samples[0u];
+    }
+
+    for( std::size_t i = 0; i < currentSize; ++i )
+    {
+        const float currentValue = m_samples[i];
+
+        m_average += currentValue;
+
+        if( i == 0u )
+        {
+            continue;
+        }
+
+        m_minValue = std::min( m_minValue, currentValue );
+        m_maxValue = std::max( m_maxValue, currentValue );
     }
 
     m_average /= (float)currentSize;
