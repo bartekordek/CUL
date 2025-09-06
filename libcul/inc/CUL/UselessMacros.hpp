d1c4c8f95521507262de672f02ef8c3339cba6b8
Bartłomiej Kordek <bartekordek@gmail.com>
2025-09-06 10:59:16 +0200

    Remove CUL_NONCOPYABLE from UselessMacros. Promotion?

--- libcul/inc/CUL/UselessMacros.hpp
+++ libcul/inc/CUL/UselessMacros.hpp
@@ -4,9 +4,3 @@
 #define NAMESPACE_END( name ) }// name
 
 
-#define CUL_NONCOPYABLE( TypeName ) \
-public: \
-TypeName( TypeName&& ) = delete;                 \
-TypeName( const TypeName& ) = delete;            \
-TypeName& operator=( const TypeName& ) = delete; \
-TypeName& operator=( TypeName&& ) = delete;
