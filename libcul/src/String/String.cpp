70f252042f1fddbb016931f460291d1c98ce47dc
Bartłomiej Kordek <bartekordek@gmail.com>
2026-01-06 20:08:08 +0100

    Cast to remove warning.

--- libcul/src/String/String.cpp
+++ libcul/src/String/String.cpp
@@ -1829,7 +1829,7 @@ Length String::wideStringToChar( std::string& out, const std::wstring& inChar )
 {
     std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converterX;
     out = converterX.to_bytes( inChar );
-    return out.size();
+    return static_cast<Length>( out.size() );
 }
 
 Length String::charToWideString( Length codePage, wchar_t* out, Length outSize, const char* in )
