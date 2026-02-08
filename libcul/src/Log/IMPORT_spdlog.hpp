733e25d3d17fc770a56eb1b776b0c745d80fd7b8
Bartłomiej Kordek <bartekordek@gmail.com>
2026-02-08 20:25:56 +0100

    Add some sinks.

--- libcul/src/Log/IMPORT_spdlog.hpp
+++ libcul/src/Log/IMPORT_spdlog.hpp
@@ -4,11 +4,12 @@
 CUL_MSVC_SKIP_DEFAULT_WARNING_BEGIN
 
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/msvc_sink.h"
-#include "spdlog/sinks/wincolor_sink.h"
 #include "spdlog/sinks/ansicolor_sink.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/msvc_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/wincolor_sink.h"
 
 #ifdef _MSC_VER
-#pragma warning( pop )
+    #pragma warning( pop )
 #endif
