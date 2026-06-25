ce1527e1bc0b4f425683ff460dd06d8512e43b24
Bartłomiej Kordek <bartekordek@gmail.com>
2026-06-25 10:07:00 +0200

    DLL linkage per method, not every mamber.

--- libcul/inc/CUL/Time.hpp
+++ libcul/inc/CUL/Time.hpp
@@ -8,12 +8,10 @@
 #include <CUL/STL_IMPORTS/STD_memory.hpp>
 #include <CUL/STL_IMPORTS/STD_future.hpp>
 
-
 namespace jed_utils
 {
 class datetime;
 }
-
 
 NAMESPACE_BEGIN( CUL )
 
@@ -39,71 +37,76 @@ struct CULLib_API BasicTime
     TimeType Seconds{ 0 };
 };
 
-class CULLib_API Time final
+class Time final
 {
 public:
-    Time();
-    Time( std::int32_t inYear, std::int32_t inMonth, std::int32_t inDay, std::int32_t inHour, std::int32_t inMinute, std::int32_t inSeconds );
+    CULLib_API Time();
+    CULLib_API Time( std::int32_t inYear,
+                     std::int32_t inMonth,
+                     std::int32_t inDay,
+                     std::int32_t inHour,
+                     std::int32_t inMinute,
+                     std::int32_t inSeconds );
 
-    Time( const Time& arg );
-    Time( Time&& arg );
-    Time& operator=( const Time& arg );
-    Time& operator=( Time&& arg );
+    CULLib_API Time( const Time& arg );
+    CULLib_API Time( Time&& arg );
+    CULLib_API Time& operator=( const Time& arg );
+    CULLib_API Time& operator=( Time&& arg );
 
-    void setTimeMs( std::int64_t time );
-    void setTimeUs( std::int64_t us );
-    void setTimeNs( std::int64_t us );
-    void setTimeSec( std::time_t sec );
-    float getMs() const;
-    float getS() const;
-    float getM() const;
-    float getH() const;
-    float getUs() const;
+    CULLib_API void setTimeMs( std::int64_t time );
+    CULLib_API void setTimeUs( std::int64_t us );
+    CULLib_API void setTimeNs( std::int64_t us );
+    CULLib_API void setTimeSec( std::time_t sec );
+    CULLib_API float getMs() const;
+    CULLib_API float getS() const;
+    CULLib_API float getM() const;
+    CULLib_API float getH() const;
+    CULLib_API float getUs() const;
 
-    void setDate( TimeType inYear, TimeType inMonth, TimeType inDay );
+    CULLib_API void setDate( TimeType inYear, TimeType inMonth, TimeType inDay );
 
-    TimeType getYear() const;
-    void setYear( TimeType inYear );
+    CULLib_API TimeType getYear() const;
+    CULLib_API void setYear( TimeType inYear );
 
-    TimeType getMonth() const;
-    void setMonth( TimeType inMonth );
+    CULLib_API TimeType getMonth() const;
+    CULLib_API void setMonth( TimeType inMonth );
 
-    TimeType getDay() const;
-    void setDay( TimeType inDay );
+    CULLib_API TimeType getDay() const;
+    CULLib_API void setDay( TimeType inDay );
 
-    TimeType getHour() const;
-    void setHour( TimeType inHour );
+    CULLib_API TimeType getHour() const;
+    CULLib_API void setHour( TimeType inHour );
 
-    TimeType getMinute() const;
-    void setMinute( TimeType inMinute );
+    CULLib_API TimeType getMinute() const;
+    CULLib_API void setMinute( TimeType inMinute );
 
-    TimeType getSecond() const;
-    void setSecond( TimeType inSecond );
+    CULLib_API TimeType getSecond() const;
+    CULLib_API void setSecond( TimeType inSecond );
 
-    void fromEpoch( std::uint64_t inSinceEpoch );
+    CULLib_API void fromEpoch( std::uint64_t inSinceEpoch );
 
-    bool operator==( const Time& ) const;
-    bool operator<( const Time& ) const;
-    bool operator>( const Time& ) const;
+    CULLib_API bool operator==( const Time& ) const;
+    CULLib_API bool operator<( const Time& ) const;
+    CULLib_API bool operator>( const Time& ) const;
 
-    Time* copy() const;
-    const StringWr& toString() const;
-    const char* cStr() const;
-    void fromString( const StringWr& inString );
+    CULLib_API Time* copy() const;
+    CULLib_API const StringWr& toString() const;
+    CULLib_API const char* cStr() const;
+    CULLib_API void fromString( const StringWr& inString );
 
     static std::uint64_t dateTimeToEpoch( const BasicTime& inBt );
 
-    BasicTime operator-( const Time& arg ) const;
+    CULLib_API BasicTime operator-( const Time& arg ) const;
 
-    bool almostTheSame( const Time& arg, std::int32_t differenceInSeconds) const;
+    CULLib_API bool almostTheSame( const Time& arg,
+                                   std::int32_t differenceInSeconds ) const;
 
-    ~Time();
+    CULLib_API ~Time();
 
 protected:
 private:
     void updateString() const;
     void waitForDateTime() const;
-
 
     float m_ns{ 0.f };
     std::unique_ptr<jed_utils::datetime> m_dateTime;
