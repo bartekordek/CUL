2be81f8ed9cf0df4e0fb2d51039713d0b2cb6930
Bartłomiej Kordek <bartekordek@gmail.com>
2026-06-28 11:20:36 +0200

    Replace namespaces.

--- libcul/inc/CUL/Filesystem/FileDatabase.hpp
+++ libcul/inc/CUL/Filesystem/FileDatabase.hpp
@@ -19,11 +19,13 @@ struct sqlite3;
 
 struct ListAndApi;
 
-NAMESPACE_BEGIN( CUL )
+namespace CUL
+{
 
 class TaskCallback;
 
-NAMESPACE_BEGIN( FS )
+namespace FS
+{
 
 using Str = STDStringWrapper;
 using MD5Value = Str;
@@ -99,7 +101,7 @@ private:
     mutable std::map<std::uint64_t, MD5List> m_groupsBySize;
 };
 
-struct CacheUsage
+struct CULLib_API CacheUsage
 {
     float Percentage{ 0.f };
     std::uint64_t Curr{ 0u };
@@ -114,7 +116,10 @@ public:
     void loadFilesFromDatabase( const Path& dbPath );
     void loadFilesFromDatabase();
 
-    void addFile( MD5Value md5, const CUL::StringWr& filePath, const CUL::StringWr& fileSize, const CUL::StringWr& modTime );
+    void addFile( MD5Value md5,
+                  const CUL::StringWr& filePath,
+                  const CUL::StringWr& fileSize,
+                  const CUL::StringWr& modTime );
 
     void removeFileFromDB( const CUL::StringWr& path );
     void removeFilesFromDb( const std::vector<CUL::StringWr>& paths );
@@ -125,13 +130,18 @@ public:
     void getListOfSizes( std::vector<uint64_t>& out ) const;
     std::vector<CUL::StringWr> getListOfMd5() const;
     std::vector<CUL::StringWr> getListOfMd5( std::uint64_t inSize ) const;
-    void getFiles( uint64_t size, const CUL::StringWr& md5, std::vector<FileInfo>& out ) const;
+    void getFiles( uint64_t size,
+                   const CUL::StringWr& md5,
+                   std::vector<FileInfo>& out ) const;
     void getFiles( uint64_t size, std::vector<FileInfo>& out ) const;
 
-    void getFilesMatching( const CUL::StringWr& fileSize, const CUL::StringWr& md5, std::list<CUL::StringWr>& out ) const;
+    void getFilesMatching( const CUL::StringWr& fileSize,
+                           const CUL::StringWr& md5,
+                           std::list<CUL::StringWr>& out ) const;
     CacheUsage getCacheUsage() const;
 
-    void getListOfPossibleDuplicates( SortedStructuredListOfFiles& inOutPossibleDuplicates );
+    void getListOfPossibleDuplicates(
+        SortedStructuredListOfFiles& inOutPossibleDuplicates );
 
     ~FileDatabase();
 
@@ -142,18 +152,24 @@ private:
     bool m_initialized = false;
     int64_t getFileCount() const;
     void updateCache();
-    void getFilesFromDB( uint64_t size, const CUL::StringWr& md5, std::vector<FileInfo>& out ) const;
+    void getFilesFromDB( uint64_t size,
+                         const CUL::StringWr& md5,
+                         std::vector<FileInfo>& out ) const;
     void getFilesFromDB( uint64_t size, std::vector<FileInfo>& out ) const;
 
     void getListOfSizesFromDb( std::vector<uint64_t>& out ) const;
     std::optional<FileInfo> getFromCache( const StringWr& inFilePath ) const;
     bool removeFromCache( const StringWr& inFilePath );
-    void addFileImpl( ELockType inLockType, MD5Value md5, const CUL::StringWr& filePath, const CUL::StringWr& fileSize,
+    void addFileImpl( ELockType inLockType,
+                      MD5Value md5,
+                      const CUL::StringWr& filePath,
+                      const CUL::StringWr& fileSize,
                       const CUL::StringWr& modTime );
     void removeFileFromDB_Impl( const CUL::StringWr& path );
     void addToCache( const FileInfo& inFile ) const;
     void addToCache( const std::vector<FileInfo>& inFiles ) const;
-    std::optional<FileInfo> getFileInfo_Impl( ELockType inLockType, const CUL::StringWr& path ) const;
+    std::optional<FileInfo> getFileInfo_Impl( ELockType inLockType,
+                                              const CUL::StringWr& path ) const;
 
     static StringWr sanitize( const StringWr& inString );
     static StringWr deSanitize( const StringWr& inString );
@@ -175,7 +191,8 @@ private:
     ListAndApi* m_fetchList = nullptr;
     std::mutex m_fetchListMtx;
 
-    mutable std::unordered_map<std::int64_t, std::unordered_map<std::string, HashGroup>> m_cachedFiles;
+    mutable std::unordered_map<std::int64_t, std::unordered_map<std::string, HashGroup>>
+        m_cachedFiles;
     std::size_t m_cachedFilesMax{ 0u };
     mutable std::mutex m_cachedFilesMtx;
 
@@ -196,5 +213,5 @@ private:
     FileDatabase& operator=( FileDatabase&& rhv ) = delete;
 };
 
-NAMESPACE_END( FS )
-NAMESPACE_END( CUL )
+}  // namespace FS
+}  // namespace CUL
